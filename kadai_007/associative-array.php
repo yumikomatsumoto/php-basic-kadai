4a11536ee7364f1cec41ad0ad46fd6a939eca117
Mattsun <39019388+yumikomatsumoto@users.noreply.github.com>
2024-05-20 12:05:54 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP課題07</title>
+</head>
+<body>
+<p>
+  <?php
+// 連想配列を作成
+$vegetable_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+// 配列を出力
+print_r($vegetable_data);
+  ?>
+</p>
+</body>
+</html>
